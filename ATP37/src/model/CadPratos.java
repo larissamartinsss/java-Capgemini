53b158f06cf2447c43cf8e6d79d4cefdc0cea028
Larissa Martins <larissamartins.job@gmail.com>
2021-11-03 09:29:25 -0400

    Cadastro de pratos

---
+++ ATP37/src/model/CadPratos.java
@@ -0,0 +1,8 @@
+package model;
+
+public class CadPratos extends Base{
+    public String nome;
+    public String descricao;
+    public double valor;
+    public Categoria categoria;
+}
---
+++ ATP37/src/model/Categoria.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Categoria extends Base{
+    public String sobremesa;
+    public String almoco;
+    public String janta;
+}
