f7461212dbde50ca6465da22c29e412fbb864190
Larissa Martins <larissamartins.job@gmail.com>
2021-11-03 13:48:46 -0400

    Atividade concluída com sucesso.

--- Atividades/ATP38/src/Leitura.java
+++ Atividades/ATP38/src/Leitura.java
@@ -1,2 +1,27 @@
+
+import java.io.*;
+
 public class Leitura {
+    public static void main(String[] args) {
+
+            try {
+                //Entrando em bites:
+                FileInputStream fis = new FileInputStream("src/dados/entrada.txt");
+                // Convertendo para Char:
+                InputStreamReader isr = new InputStreamReader(fis);
+                // Acumulando Chars para leitura
+                BufferedReader br = new BufferedReader(isr);
+
+               while(br.ready()){
+                   System.out.println("\n"+br.readLine());
+               }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Arquivo não encontrado");
+            } catch (IOException e) {
+                System.out.println("Não é possivel ler o arquivo.");
+            }
+
+
+    }
 }
