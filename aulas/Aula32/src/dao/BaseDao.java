2c0d84884a2cfaf5b27ef6431d1e9d2dff1560fe
Larissa Martins <larissamartins.job@gmail.com>
2021-11-04 11:10:51 -0400

    Intervalo

--- aulas/Aula32/src/dao/BaseDao.java
+++ aulas/Aula32/src/dao/BaseDao.java
@@ -1,6 +1,9 @@
 package dao;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class BaseDao<T> {
     private String filename;
@@ -16,11 +19,20 @@ public class BaseDao<T> {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void list(){
+        try {
+            Scanner sc = new Scanner(new File(this.filename));
+            while (sc.hasNextLine()){
+                String linha = sc.nextLine();
 
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update(){
--- aulas/Aula32/src/model/Pessoa.java
+++ aulas/Aula32/src/model/Pessoa.java
@@ -4,13 +4,19 @@ public class Pessoa extends BaseModel{
     public String nome;
     public String sobrenome;
 
+    public Pessoa(String pessoaString){
+       String[] dados =  pessoaString.split(";");
+       this.id = Integer.parseInt(dados[0]);
+       this.nome = dados[1];
+       this.sobrenome = dados[2];
+    }
+
     @Override
     public String toString() {
         String retorno = String.format("%s; %s; %s;",
                 super.toString(),
                 this.nome,
                 this.sobrenome);
-
         return retorno;
     }
 }
