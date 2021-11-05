443bb5510bface7aa92e102d5d0b540d92179fb5
Larissa Martins <larissamartins.job@gmail.com>
2021-11-05 10:05:04 -0400

    Atividade 40

--- Atividades/ATP40/src/cadEmpresas/view.java
+++ Atividades/ATP40/src/cadEmpresas/view.java
@@ -1,7 +1,9 @@
 package cadEmpresas;
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class view {
     public static void main(String[] args) {
@@ -14,9 +16,22 @@ public class view {
             FileWriter fw = new FileWriter("src/dados/empresa.csv");
             String empresaString = String.format("%s;%s;%s", empresa.nome, empresa.cnpj, empresa.ramo);
             fw.write(empresaString);
+            fw.close();
         } catch (IOException e) {
             System.out.println("Não foi possível abrir o arquivo. ");
         }
+
+        try {
+           Scanner sc = new Scanner(new File("src/dados/empresa.csv"));
+            while (sc.hasNextLine()) {
+                String linha = sc.nextLine();
+                String[] empresaString = linha.split(";");
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
