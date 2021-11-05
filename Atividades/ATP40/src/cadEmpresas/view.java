044794d796d7dd5708ad41738d87714a108d5b89
Larissa Martins <larissamartins.job@gmail.com>
2021-11-05 10:31:01 -0400

    Revisando ativdade com o prof.

--- Atividades/ATP40/src/cadEmpresas/view.java
+++ Atividades/ATP40/src/cadEmpresas/view.java
@@ -6,27 +6,39 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class view {
+    private static String fileName = "src/dados/empresa.csv";
     public static void main(String[] args) {
         Empresa empresa = new Empresa();
         empresa.nome = "lariTech";
         empresa.cnpj = "XX. XXX. XXX/0001-XX";
         empresa.ramo = "Desenv. de Softwares";
 
+        //DAO - Object -> String
+        String empresaString = String.format("%s;%s;%s\n", empresa.nome, empresa.cnpj, empresa.ramo);
+                            // String formart é mais performatico.
+
+
         try {
-            FileWriter fw = new FileWriter("src/dados/empresa.csv");
-            String empresaString = String.format("%s;%s;%s", empresa.nome, empresa.cnpj, empresa.ramo);
-            fw.write(empresaString);
+            FileWriter fw = new FileWriter(fileName, true);
+           fw.write(empresaString);
             fw.close();
         } catch (IOException e) {
             System.out.println("Não foi possível abrir o arquivo. ");
         }
-
+        // leitura
         try {
-           Scanner sc = new Scanner(new File("src/dados/empresa.csv"));
+           Scanner sc = new Scanner(new File(fileName));
             while (sc.hasNextLine()) {
                 String linha = sc.nextLine();
-                String[] empresaString = linha.split(";");
+                String[] dadosEmpresa = linha.split(";");
                 System.out.println(sc.nextLine());
+                // DAO - STRING --> OBJECT
+                Empresa e = new Empresa();
+                e.nome =dadosEmpresa[0];
+                e.cnpj =dadosEmpresa[1];
+                e.ramo =dadosEmpresa[2];
+                // PRINT DO OBJECT FORMATADO:
+                System.out.printf("\"%s;%s;%s\\n\"", e.nome, e.cnpj, e.ramo);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
