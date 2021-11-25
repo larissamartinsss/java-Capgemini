eb0022a9b0f253daeab4c74970e7ddb5a864191d
Larissa Martins <larissamartins.job@gmail.com>
2021-11-25 12:01:08 -0400

    ATP47

--- Atividades/ATP57/vendas/src/main/java/com/lariscode/vendas/servlets/CategoriaReadServlet.java
+++ Atividades/ATP57/vendas/src/main/java/com/lariscode/vendas/servlets/CategoriaReadServlet.java
@@ -1,11 +1,12 @@
 package com.lariscode.vendas.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import com.lariscode.vendas.dao.CategoriaDao;
 import com.lariscode.vendas.models.Categoria;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,11 +17,16 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CategoriaReadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
+        
         CategoriaDao dao = new CategoriaDao();
-        for (Categoria model : dao.read()) {
-            out.printf("%d - %s\n", model.getId(), model.getNome());
-        }
+         //for (Categoria model : dao.read()) {
+         //   out.printf("%d - %s\n", model.getId(), model.getNome());
+        // }
+        
+        ArrayList<Categoria> categorias = dao.read();
+
+        RequestDispatcher rd = req.getRequestDispatcher("/categoria-lista.jsp");
+        rd.forward(req, resp);
     }
     
 }
---
+++ Atividades/ATP57/vendas/src/main/webapp/categoria-lista.jsp
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>categoria-lista</title>
+</head>
+<body>
+    <h1>Lista de categorias</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Cat1</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
