b4a4bebfd957888baac982100f8a8ba24d31f8db
Larissa Martins <larissamartins.job@gmail.com>
2021-11-25 09:30:52 -0400

    ATP46 - Atividade concluída!!!!!

---
+++ Atividades/ATP56/vendas/src/main/java/com/lariscode/vendas/servlets/CategoriaReadServlet.java
@@ -0,0 +1,26 @@
+package com.lariscode.vendas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.lariscode.vendas.dao.CategoriaDao;
+import com.lariscode.vendas.models.Categoria;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/categoria/listar")
+public class CategoriaReadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        CategoriaDao dao = new CategoriaDao();
+        for (Categoria model : dao.read()) {
+            out.printf("%d - %s\n", model.getId(), model.getNome());
+        }
+    }
+    
+}
