4a834fe42e30b0c49a77b49e471d6d169d5c2f43
Larissa Martins <larissamartins.job@gmail.com>
2021-11-29 20:28:53 -0400

    ATP65

---
+++ Atividades/ATP65/Clientes/src/main/java/com/lariscode/Clientes/model/CadClientes.java
@@ -0,0 +1,31 @@
+package com.lariscode.Clientes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cliente")
+public class CadClientes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private int id;
+    @Getter @Setter
+
+    private String nome;
+    @Getter @Setter
+    private String sobrenome;
+
+    @Getter @Setter
+    private int idade;
+    
+    @Getter @Setter
+    private String genero;
+
+}
