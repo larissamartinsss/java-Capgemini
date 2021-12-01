475ddfdc0d0602cd0f8317f52d3db51d11706cd1
Larissa Martins <larissamartins.job@gmail.com>
2021-12-01 09:17:59 -0400

    Iniciando ATP67

--- Aulas/aula47/filmes/src/main/java/com/lariscode/filmes/api/FilmeApi.java
+++ Aulas/aula47/filmes/src/main/java/com/lariscode/filmes/api/FilmeApi.java
@@ -6,39 +6,47 @@ import com.lariscode.filmes.model.Filme;
 import com.lariscode.filmes.repository.FilmeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api/filme")
 public class FilmeApi {
     @Autowired
     private FilmeRepository repository;
     
-    @GetMapping("/api/filme")
-    public List<Filme> filmes(){
+    @GetMapping
+    public List<Filme> filmes(String nome){
         List<Filme> lista = (List<Filme>)repository.findAll();
+    
+        if(nome != null){
+            return List<Filme> 
+        }
+
         return lista;        
     }
     
-    @PostMapping("/api/filme")    
+    @PostMapping
     public String salvar(@RequestBody Filme model){
         repository.save(model);
         return "salvo com sucesso";
     }
 
-    @DeleteMapping("/api/filme/{id}")    
+    @DeleteMapping("/{id}")    
     public String deletar(@PathVariable int id){
         repository.deleteById(id);
         return "deletado com sucesso";
     }
 
-    @PutMapping("/api/filme/{id}")    
+    @PutMapping("/{id}")    
     public String update(@RequestBody Filme model, @PathVariable int id){
         if(model.getId() == id){
             repository.save(model);
--- Aulas/aula47/filmes/src/main/java/com/lariscode/filmes/repository/FilmeRepository.java
+++ Aulas/aula47/filmes/src/main/java/com/lariscode/filmes/repository/FilmeRepository.java
@@ -2,8 +2,14 @@ package com.lariscode.filmes.repository;
 
 import com.lariscode.filmes.model.Filme;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FilmeRepository extends CrudRepository<Filme, Integer> {
+public interface FilmeRepository extends PagingAndSortingRepository<Filme, Integer> {
     
+    @Override
+    default Iterable<Filme> findAll() {
+        return findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+
 }
